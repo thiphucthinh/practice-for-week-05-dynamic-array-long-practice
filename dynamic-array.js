c8806cf41f4266b97d21e2e1a3c409fdc2b85c6a
Thi Phuc Thinh Huynh <ph.thinh905@gmail.com>
2023-09-23 20:22:44 -0700

    Finish resize method

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,44 +1,67 @@
 class DynamicArray {
 
-  constructor(defaultSize = 4) {
-
-    // Your code here
+  constructor(capacity= 4) {
+    this.capacity = capacity;
+    this.length = 0;
+    this.data = new Array(capacity);
   }
 
   read(index) {
-
-    // Your code here
+    return this.data[index];
   }
 
   push(val) {
+    if (this.length === this.capacity) { this.resize() }
 
-    // Your code here
+    this.data[this.length] = val;
+    this.length++;
   }
 
 
   pop() {
-
-    // Your code here
+    if (this.length === 0) return undefined;
+    const poppedValue = this.data[this.length - 1];
+    this.data[this.length - 1] = undefined;
+    this.length--;
+    return poppedValue;
   }
 
   shift() {
+    if (this.length === 0) return undefined;
+    const shiftedValue = this.data[0];
+    for (let i = 0; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
 
-    // Your code here
+    this.data[this.length - 1] = undefined;
+    this.length--;
+    return shiftedValue;
   }
 
   unshift(val) {
+    if (this.length === this.capacity) { this.resize() }
 
-    // Your code here
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = val;
+    this.length++;
   }
 
   indexOf(val) {
-
-    // Your code here
+    for (let i = 0; i < this.length; i++) {
+      if (this.data[i] === val) return i;
+    }
+    return -1;
   }
 
   resize() {
-
-    // Your code here
+    this.capacity *= 2;
+    const newData = new Array(this.capacity);
+    for (let i = 0; i < this.length; i++) {
+      newData[i] = this.data[i];
+    }
+    this.data = newData;
   }
 
 }
