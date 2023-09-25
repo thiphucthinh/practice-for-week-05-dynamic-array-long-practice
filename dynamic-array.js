dd8e1b0f1cd3b07a50a964d6ecf147a7cdc198c2
Thi Phuc Thinh Huynh <ph.thinh905@gmail.com>
2023-09-24 21:51:54 -0700

    Modify some lines

--- dynamic-array.js
+++ dynamic-array.js
@@ -11,41 +11,44 @@ class DynamicArray {
   }
 
   push(val) {
-    if (this.length === this.capacity) { this.resize() }
+    if (this.length === this.capacity) { this.resize() };
 
     this.data[this.length] = val;
     this.length++;
+    return this.length;
   }
 
 
   pop() {
     if (this.length === 0) return undefined;
+
     const poppedValue = this.data[this.length - 1];
-    this.data[this.length - 1] = undefined;
     this.length--;
     return poppedValue;
   }
 
   shift() {
     if (this.length === 0) return undefined;
+
     const shiftedValue = this.data[0];
     for (let i = 0; i < this.length; i++) {
       this.data[i] = this.data[i + 1];
     }
 
-    this.data[this.length - 1] = undefined;
     this.length--;
     return shiftedValue;
   }
 
   unshift(val) {
-    if (this.length === this.capacity) { this.resize() }
+    if (this.length === this.capacity) { this.resize() };
 
     for (let i = this.length; i > 0; i--) {
       this.data[i] = this.data[i - 1];
     }
+
     this.data[0] = val;
     this.length++;
+    return this.length;
   }
 
   indexOf(val) {
